9a1965e012a46b45fb2117fc6a4d2d082a4b9404
Raits Misins <raits.misins@yahoo.com>
2023-01-09 09:33:49 +0200

    prepared the necessary source code files for further work.

---
+++ main.py
@@ -0,0 +1,15 @@
+import turtle
+import draw_enhanced as draw
+
+
+
+def main():
+    # SCR_SIZE = 1100    
+    # turtle.setup(width=SCR_SIZE, height=SCR_SIZE, startx=0, starty=0) #, startx=MARGIN, starty=MARGIN)
+    # turtle.screensize(canvwidth=SCR_SIZE, canvheight=SCR_SIZE,bg="black") #  
+
+    draw.test2()
+
+if __name__ == "__main__":
+    main()
+  
