3002e3f1a9a6b7a1e04eb51976674ae1d4eb848e
Raits Misins <raits.misins@yahoo.com>
2023-01-21 13:19:12 +0200

    Nearly final version

--- draw_enhanced.py
+++ draw_enhanced.py
@@ -1,6 +1,8 @@
 from abc import *
 from abc import *
 import turtle
+import math
+
 
 class Canvas:
     def __init__(self, w, h):
@@ -87,6 +89,11 @@ class Shape(GeometricObject):
     def getFillColor(self):
         return self.__fillColor
 
+    @abstractmethod
+    def _draw(self):
+        pass
+
+
 
 # coords parameter for Point class is a tuple (x,y)
 class Point(GeometricObject):
@@ -108,11 +115,17 @@ class Point(GeometricObject):
         # return self.__y
         return self.__coordinates[1]
     
+    def setCoordinates(self,x,y):
+        self.__coordinates(x,y)
+    
     def _draw(self, turtle):
         # turtle.goto(self.__x, self.__y)
        # turtle.dot(self.__lineWidth, self.__lineColor)
         turtle.goto(self.__coordinates[0], self.__coordinates[1])
         turtle.dot(self.getWidth(), self.getColor())
+    
+    def __eq__(self, other):
+        return self.getCoord() == other.getCoord()
 
 class Line(GeometricObject):
     def __init__(self, p1, p2):
@@ -135,56 +148,133 @@ class Line(GeometricObject):
         turtle.goto(self.__p2.getCoord())
 
 
-
-
 class Polygon(Shape):
     # parameters: vertices (list of tuples)
     #
     def __init__(self, corners : list):
-        super(Polygon,self).__init__()
+        # check if any corners are 
+        t = corners[0]
+        # check if all corners have different coordinates
+        for i in range(1,len(corners)-1):
+            if t == corners[i]:
+                raise ValueError("Polygon: One or more corners have same coordinates")
+            else:
+                t = corners[i]
+        if not self.__isconvex(corners):
+            raise ValueError("Polygon: Must be a convex shape.")
+        super().__init__()
         self.__corners = corners
+
+    def __isconvex(self, corners) -> bool:
+    # checks if the polygon is convex
+    # not implemented yet    
+        return True
     
     def _draw(self, turtle):
+        # print("****")
         turtle.color(self.getColor())
         turtle.width(self.getWidth())
         turtle.up()
+        k = 0
         start_point = (self.__corners[0].getCoord())
-
         for c in self.__corners:
-            turtle.goto(c.getCoord())
+            turtle.goto(c.getX(),c.getY()) # (c.getCoord())
             turtle.down()
-        turtle.goto(start_point)            
+            # turtle.dot(8,"red")
+            # k += 1
+            # print(k, c.getCoord())
+            # print(turtle.xcor(), turtle.ycor())
+        turtle.goto(self.__corners[0].getX(), self.__corners[0].getY())  #start_point)            
+        # print(turtle.xcor(), turtle.ycor())
         turtle.up()
 
+    def getAllCorners(self) -> list:
+        return self.__corners
+    
+    def setAllCorners(self, corners : list):
+        if len(corners) != len(self.__corners):
+            raise ValueError("Polygon: number of corners must stay unchanged")
+        self.__corners = corners        
+    
+    def getCornerN(self, corner_number) -> Point:
+        # retrieves coordinates of a corner N for an X-corner polygon. N must be between 1 and X.
+        if corner_number <= 0 or corner_number > len(self.__corners):
+            raise ValueError("Polygon: corner number out of range. The values must be within 1 and "+str(len(self.__corners))+".")
+        else:
+            return self.__corners[corner_number-1]
+
+    def setCornerN(self, corner_number, new_point : Point):
+        # sets coordinates of a corner N for an X corner polygon. N must be between 1 and X.
+        if corner_number <= 0 or corner_number > len(self.__corners):
+            raise ValueError("Polygon: corner number out of range. The values must be within 1 and "+str(len(self.__corners))+".")
+        else:
+            self.__corners[corner_number-1] = new_point
+        
+
 class Triangle(Polygon):
+    # A convenience class for Triangle
+    # __init__ takes a list of three items of Point type to define a triangle
+
+    # Limitations: 
+    # a) There is no check if all three corners are on the same line
+    
     def __init__(self, corners : list):
         if len(corners) != 3:
             raise ValueError("Triangle: wrong number of corners. must be three.")
         else:
-            super(Triangle,self).__init__(corners)
+            super().__init__(corners)
 
 class Rectangle(Polygon):
-    pass
+    # A convenience class for rectangles
+    # the __init__ takes: 
+    # a) the coordinates of the lower-left corner through a Point object
+    # b) width (horizontal length) and height (vertical length) of the rectangle
+    # c) rotation angle (optional, 0 if not specified)
+    def __init__(self, ll: Point, width : int, height : int, rotation_angle = 0):
+        if width <= 0 or height <= 0:
+            raise ValueError("Rectangle: Height or width cannot be zero.")
+        else:
+        # proceed building the rectangle
+            corners = [ll]      # add lower left corner to the rectangle
+            corners.append(Point(ll.getX() + width, ll.getY()))   # add lower right (x + width, same height)
+            corners.append(Point(ll.getX() + width, ll.getY() + height)) # add upper right (x + width, y + height)
+            corners.append(Point(ll.getX(), ll.getY() + height))
+            if rotation_angle != 0:
+                corners = self.__rotate(corners, rotation_angle)
+            print(len(corners))
+            super().__init__(corners)
+
+    def __rotate(self, corner_array : list, angle ) -> list:
+    # the rotation angle shall be specified in degrees
+    # get x and y for LL corner:
+        x_base = corner_array[0].getX()
+        y_base = corner_array[0].getY()
+
+        for n in range(1,len(corner_array)):
+            x_current = corner_array[n].getX()
+            y_current = corner_array[n].getY()
+            
+            r = math.sqrt((x_current - x_base)**2 + (y_current - y_base)**2)
+
+            # calculate base angle
+            if x_current == x_base:
+                base_angle = 90     # straight angle
+            else:
+                base_angle = math.degrees(math.atan((y_current - y_base) / (x_current - x_base)))
+            # the actual rotation angle will be base angle plus rotation angle            
+            x_new = x_base + r * math.cos(math.radians(angle + base_angle))
+            y_new = y_base + r * math.sin(math.radians(angle + base_angle))
+            del corner_array[n]
+            corner_array.insert(n,Point(x_new, y_new))
+        return corner_array
+
 
 class Octagon(Polygon):
-    pass
-
     
-
-
-
-def test2():
-    myCanvas = Canvas(500, 500)
-    line1 = Line(Point(-100, -100), Point(100, 100))
-    line2 = Line(Point(-100, 100), Point(100, -100))
-    # line1.setWidth(4)    
-    myCanvas.draw(line1)
-    myCanvas.draw(line2)
-    line1.setColor('red')
-    line2.setWidth(4)
-    t1 = Triangle([Point(-50,-50), Point(50,50), Point(50,0)])
-    t1.setWidth(2)
-    t1.setColor('yellow')
-    myCanvas.draw(t1)
-    turtle.done()
-
+    def __init__(self, corners: list):
+        # check if number of corners is 8
+        if (len(corners) !=8):
+            raise ValueError("Rectangle: wrong number of corners. Eight corners expected.")
+        else:
+            super().__init__(corners)
+        
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import turtle
 import draw_enhanced as draw
-
+import math
 
 
 def main():
@@ -8,8 +8,70 @@ def main():
     # turtle.setup(width=SCR_SIZE, height=SCR_SIZE, startx=0, starty=0) #, startx=MARGIN, starty=MARGIN)
     # turtle.screensize(canvwidth=SCR_SIZE, canvheight=SCR_SIZE,bg="black") #  
 
-    draw.test2()
+    # draw.test2()
+    shape_list = []
+    myCanvas = draw.Canvas(500, 500)
+    
+    line1 = draw.Line(draw.Point(-100, -100), draw.Point(100, 100))
+    line1.setWidth(4)
+    shape_list.append(line1)
+
+    line2 = draw.Line(draw.Point(-100, 100), draw.Point(100, -100))
+    shape_list.append(line2)
+
+
+    t1 = draw.Triangle([draw.Point(-50,-50), draw.Point(50,50), draw.Point(50,50)])
+    t1 = draw.Triangle([draw.Point(-50,-50), draw.Point(50,50), draw.Point(50,0)])
+    t1.setWidth(2)
+    t1.setColor('green')
+    shape_list.append(t1)
+
+    t1 = draw.Triangle([draw.Point(50,-50), draw.Point(150,-50), draw.Point(50,-20)])
+    t1.setWidth(2)
+    t1.setColor('red')
+    shape_list.append(t1)
+        
+    r1 = draw.Rectangle(draw.Point(0,0),25,150,75)
+    r1.setWidth(2)
+    r1.setColor('blue')
+    shape_list.append(r1)
+  
+    r2 = draw.Rectangle(draw.Point(100,0),100,140, 45)
+    shape_list.append(r2)
+    
+    o1 = draw.Octagon([draw.Point(-200,30),draw.Point(-140,80),draw.Point(150,30),draw.Point(200,0),
+                       draw.Point(160,-60),draw.Point(0,-30),draw.Point(-150,0),draw.Point(-200,20)])
+    o1.setWidth(3)
+    o1.setColor('yellow')
+
+    shape_list.append(o1)
+    
+    # draw all shapes    
+    for sh in shape_list:
+        myCanvas.draw(sh)
+
+    line1.setColor('red')
+    line2.setWidth(4)
+    line2.setColor('green')
+    turtle.done()
+
+    return
+    
+    # test sinus and cosinus calculation
+def test_fn():
+    x_base = 0
+    y_base = 0
+    x_current = 160
+    y_current = 0
+    angle = 30
+    r = math.sqrt((x_current - x_base)**2 + (y_current - y_base)**2)
+    print(r)
+    print(math.radians(angle))
+    x_new = r * math.cos(math.radians(angle))
+    y_new = r * math.sin(math.radians(angle))
+    print(x_new, y_new)
 
 if __name__ == "__main__":
+    test_fn()
     main()
   
